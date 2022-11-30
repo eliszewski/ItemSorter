e6318940af5db9768c8e216fdc7cd221b8c15be6
Eryk <eryk@Zipcoders-MacBook-Pro-71.local>
2022-11-30 18:30:42 -0500

    finished

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,34 @@
 package rocks.zipcode.io;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] itemsToSort;
     public ItemSorter(Item[] items) {
+        this.itemsToSort = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        int i,j;
+        Item temp;
+        for (i = 0; i < this.itemsToSort.length - 1 ; i++) {
+            boolean changed = false;
+            for (j = 0; j < this.itemsToSort.length - i - 1 ; j++) {
+                if(comparator.compare(this.itemsToSort[j],this.itemsToSort[j+1]) > 0){
+                    temp = this.itemsToSort[j];
+                    this.itemsToSort[j] = this.itemsToSort[j+1];
+                    this.itemsToSort[j + 1] = temp;
+                    changed = true;
+                }
+            }
+            if(!changed) break;
+        }
+        return this.itemsToSort;
     }
+
+
 }
