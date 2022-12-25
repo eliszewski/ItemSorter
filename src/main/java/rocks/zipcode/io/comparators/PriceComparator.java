1a60d41d5eac7cdc751d1dc74943f3f7dbfc6ed8
eliszewski <liszewski31098@gmail.com>
2022-12-25 08:05:18 -0500

    remove commented code

--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -10,9 +10,6 @@ import java.util.Comparator;
 public class PriceComparator implements Comparator<Item> {
     @Override
     public int compare(Item o1, Item o2) {
-//        if(o1.getPrice() - o2.getPrice() > 0) return 1;
-//        else if(o1.getPrice() - o2.getPrice() < 0) return -1;
-//        else return 0;
         return o1.getPrice().compareTo(o2.getPrice());
     }
 }
